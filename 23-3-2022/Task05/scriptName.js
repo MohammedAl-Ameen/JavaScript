5ec24dffd94da2d82fe46e958ebd799e9ee9e421
unknown <mohammedamin.ababneh@gmail.com>
2022-03-26 15:30:38 +0200

    main

--- 23-3-2022/Task05/scriptName.js
+++ 23-3-2022/Task05/scriptName.js
@@ -1,13 +1,12 @@
 tooshort1 = false;
 tooshort2 = false;
-login = false;
 
 
 function check(){
     var str1 = document.getElementById("firstpass").value;
     var str2 = document.getElementById("secondpass").value;
     
-    tooshort = tooshort1 & tooshort2;
+   
 
     if(str1.length < 6){
         document.getElementById("warning1").innerHTML = "too short";
@@ -17,6 +16,7 @@ function check(){
         document.getElementById("warning1").innerHTML = "";
         tooshort1 = false;
     }
+
     if((str2.length < 6)){
         document.getElementById("warning2").innerHTML = "too short";
         tooshort2 = true;
@@ -32,16 +32,20 @@ function check(){
 
     if(str1.length == 0){
         document.getElementById("warning1").innerHTML = "";
-        tooshort2 = false;
+        tooshort1 = false;
     }
 
-     if((str1 != str2) & (str1.length != 0) & (str2.length != 0) & !tooshort){
+    tooshort = tooshort1 && tooshort2;
+
+     if((str1 != str2) && (str1.length != 0) && (str2.length != 0) && !tooshort){
         document.getElementById("warning2").innerHTML = "the two passwords don't match";
     }
 
-    if(str1 == str2){
-        login = true;
-        document.getElementById("btn").style.display ="block";}
+    if((str1 == str2) && (!tooshort) && (str1.length != 0) && (str2.length != 0)){
+        document.getElementById("btn").style.display ="block";
+    }else{
+        document.getElementById("btn").style.display ="none";
+    }
     
 
    
